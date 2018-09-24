51b2abffa38df1f462ae9f7d1234741d10e51ec8
RVCcoders <41723548+RVCcoders@users.noreply.github.com>
2018-09-24 18:46:53 -0500

    Add files via upload

---
+++ HdeKeyBoard/HdeKeyBoard/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  HdeKeyBoard
+//
+//  Created by cis290 on 9/24/18.
+//  Copyright © 2018 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ScrollView: UIScrollView!
+ @IBOutlet weak var txtView: UITextView!
+
+    @IBOutlet weak var txtLast: UITextField!
+    @IBOutlet weak var txtFirst: UITextField!
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBAction func btnBack(_ sender: UIButton) {
+    }
+    @IBAction func btnView(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnSave(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
